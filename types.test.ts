b8abf6674cd4fe7b6da34e954b5fa4f413643b08
Espen Meidell <espen.meidell@gmail.com>
2019-11-15 09:06:15 +0100

    Add type test without options object

--- types.test.ts
+++ types.test.ts
@@ -21,4 +21,20 @@ app.register(fastifyAuth).after((err) => {
       done();
     },
   ], {relation: 'or'});
+  app.auth([
+    (
+      _request: fastify.FastifyRequest<
+        http.IncomingMessage,
+        fastify.DefaultQuery,
+        fastify.DefaultParams,
+        fastify.DefaultHeaders,
+        any
+      >,
+      _reply: fastify.FastifyReply<http.ServerResponse>,
+      done
+    ) => {
+      done(new Error());
+      done();
+    },
+  ]);
 });
