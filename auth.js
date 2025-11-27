b763667f6a06275ed6d56dfaed3025b9cc97d011
Frazer Smith <frazer.dev@icloud.com>
2025-11-27 10:37:46 +0000

    fix: avoid mutating `opts` param in `fastifyAuth`

--- auth.js
+++ auth.js
@@ -8,11 +8,13 @@ const DEFAULT_RELATION = 'or'
 function fastifyAuth (fastify, opts, next) {
   if (opts.defaultRelation && opts.defaultRelation !== 'or' && opts.defaultRelation !== 'and') {
     return next(new Error("The value of default relation should be one of ['or', 'and']"))
-  } else if (!opts.defaultRelation) {
-    opts.defaultRelation = DEFAULT_RELATION
+  }   
+  
+  const pluginOptions = {
+    defaultRelation: opts.defaultRelation || DEFAULT_RELATION
   }
 
-  fastify.decorate('auth', auth(opts))
+  fastify.decorate('auth', auth(pluginOptions))
   next()
 }
 
